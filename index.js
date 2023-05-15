c5718495d3b70704ea07a0aa8906b84f6aef178c
Gaurav Audichya <gauravaudichya786@gmail.com>
2023-05-15 18:29:55 +0530

    final today

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/add",(req,res)=>{
+
+    console.log("adduser");
+    res.send('Hello World!')
 })
+
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
